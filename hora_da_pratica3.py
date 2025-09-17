0379405dbd085771dd191e2f312b690950be3dc1
Taitwelve <taynascimento15@gmail.com>
2025-09-17 18:52:10 -0300

    fix: praticando

---
+++ hora_da_pratica3.py
@@ -0,0 +1,18 @@
+import math
+
+a = float(input("Informe o valor de A: "))
+b = float(input("Informe o valor de B: "))
+c = float(input("Informe o valor de C: "))
+#cálculo do delta
+delta = b * b - 4 * a *c
+
+if delta > 0.0:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    print("Para a equação {}x² + {}x + {} = 0, obtivemos os seguintes valores: x1 = {} e x2 = {}".format(a, b, c, x1, x2))
+else:
+    if delta == 0:
+        x = (-b + math.sqrt(delta)) / (2 * a)
+        print("Para a equação {}x² + {}x + {} = 0, obtivemos o seguinte valor: x = {}".format(a, b, c, x))
+    else:
+        print("Para a equação {}x² + {}x + {} = 0, não existem valores reais para x".format(a, b, c))
