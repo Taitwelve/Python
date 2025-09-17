b9a6de28bf89ca282cc929e0313f1e2a71e95eb5
Taitwelve <taynascimento15@gmail.com>
2025-09-17 18:31:14 -0300

    fix: exemplo com operador lógico AND

---
+++ conectivo_and.py
@@ -0,0 +1,8 @@
+valor_compra = float(input("Digite o valor da sua compra: "))
+forma_pagamento = int(input("1 - Dinheiro | 2 - Cartão : "))
+
+if valor_compra > 100 and forma_pagamento == 1 :
+    valor_compra = valor_compra * 0.9
+    print("Você tem direito a 10% de desconto!")
+
+print(f"O valor a pagar é de R${valor_compra}")    
