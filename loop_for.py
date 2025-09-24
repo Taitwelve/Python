c92255fdd19584adc0869c8f41b875f62ab1798c
Taitwelve <taynascimento15@gmail.com>
2025-09-24 15:18:01 -0300

    Exemplos de incremento de números pares e impares usando o 3º range

--- loop_for.py
+++ loop_for.py
@@ -1,9 +1,20 @@
+#a variável contadora deverá assumir valores no intervalo entre 1 e 9
 for x in range(10):
     print(x)
 
+#a variável contadora deverá assumir valores no intervalo entre 1 e 14
 for x in range(1,15):
     print(x)    
 
 #a variável contadora deverá assumir valores no intervalo entre 1 e 9 com incremento 2
 for x in range(1,10,2):
     print(x)    
+
+#a variável contadora deverá assumir valores no intervalo entre 1 e 1000 mostrandfo apenas os valores pares
+for x in range(2,1001,2):
+    print(x)
+
+#a variável contadora deverá assumir valores no intervalo entre 1 e 1000 mostrando apenas os valores ímpares
+for x in range(1,1000,2):
+    print(x)
+         
