6b95113dd3a414a83baa393b494a05b19bd7c0d3
Taitwelve <taynascimento15@gmail.com>
2025-09-24 14:54:28 -0300

    exemplos de loop for

---
+++ loop_for.py
@@ -0,0 +1,9 @@
+for x in range(10):
+    print(x)
+
+for x in range(1,15):
+    print(x)    
+
+#a variável contadora deverá assumir valores no intervalo entre 1 e 9 com incremento 2
+for x in range(1,10,2):
+    print(x)    
