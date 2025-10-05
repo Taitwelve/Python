af4870e31c335edf8f42d6735764efca41f0e42a
Taitwelve <taynascimento15@gmail.com>
2025-10-05 02:05:32 -0300

    Refactor calcularVelocidadeMedia function to remove input prompts and accept parameters

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-#Função que calcula a velocidade média
+# Função que calcula a velocidade média
 def calcularVelocidadeMedia():
     #solicitar distância e tempo
     distancia = float(input("Digite a distância percorrida em quilometros: "))
@@ -10,3 +10,30 @@ def calcularVelocidadeMedia():
 
 #aqui começa o programa principal
 calcularVelocidadeMedia()
+
+
+# NADA DE INPUT NA FUNÇÃO, APENAS PARÂMETROS 
+def calcularVelocidadeMedia(distancia, tempo):
+    #calcular a velocidade média
+    velocidade_media = distancia/tempo
+    #exibir o resultado
+    print("A velocidade média é {} km/h".format(velocidade_media))
+
+#aqui começa o programa principal
+calcularVelocidadeMedia(50, 20)   
+
+#Função que calcula a velocidade média
+def calcularVelocidadeMedia(distancia, tempo):
+    #calcular a velocidade média
+    velocidade_media = distancia/tempo
+    #exibir o resultado
+    print("A velocidade média é {} km/h".format(velocidade_media))
+
+#aqui começa o programa principal
+distancia = float(input("Informe a distância: "))
+tempo = float(input("Informe o tempo: "))
+#chamando a função com valores definidos pelo usuário
+calcularVelocidadeMedia(distancia, tempo)
+
+#chamando a função com valores definidos pelo programador
+calcularVelocidadeMedia(15,2)
