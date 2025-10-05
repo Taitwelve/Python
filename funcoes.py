d34ed378ab4fde7e87c837cfdf05460b0eecfa90
Taitwelve <taynascimento15@gmail.com>
2025-10-05 02:27:55 -0300

    Refactor calcularVelocidadeMedia function to improve structure and return value

--- funcoes.py
+++ funcoes.py
@@ -37,3 +37,16 @@ calcularVelocidadeMedia(distancia, tempo)
 
 #chamando a função com valores definidos pelo programador
 calcularVelocidadeMedia(15,2)
+
+
+#Função que calcula a velocidade média
+def calcularVelocidadeMedia(distancia, tempo):
+    #calcular a velocidade média
+    velocidade_media = distancia/tempo
+    #exibir o resultado
+    return velocidade_media
+#aqui começa o programa principal
+dist = float(input("Informe a distância: "))
+temp = float(input("Informe o tempo: "))
+#chamando a função com valores definidos pelo usuário
+print("A velocidade média é {}".format(calcularVelocidadeMedia(dist, temp)))
