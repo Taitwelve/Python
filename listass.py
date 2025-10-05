c3f5946999a9216d496450a866085f27f38181b2
Taitwelve <taynascimento15@gmail.com>
2025-10-05 02:25:47 -0300

    Refactor arithmetic functions to remove input prompts and accept parameters

--- listass.py
+++ listass.py
@@ -26,4 +26,12 @@ def dividir ():
     print("A divisão entre {} e {} é igual a {}".format(a, b, divisao))
 dividir()   
 
+# Refactored version of the sum function
+def soma (a, b):
+    somando = a + b
+    print("A soma entre {} e {} é igual a {}".format(a, b, somando))
 
+valor1 = float(input("Digite um número: "))
+valor2 = float(input("Digite outro número para somar ao primeiro: "))
+soma(valor1, valor2)    
+soma (5, 10)
