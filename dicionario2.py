014a2fea35a09e16834a290cb373ee3e8cfec792
Taitwelve <taynascimento15@gmail.com>
2025-11-12 09:30:44 -0300

    adicionando informações por input

--- dicionario2.py
+++ dicionario2.py
@@ -1,6 +1,10 @@
 dicionario = {}
 
 dicionario["Taiane Nascimento"] = "Desenvolvedora"
+nome_colaborador = input("Digite o nome do colaborador: ")
+cargo_colaborador = input("Digite o cargo do colaborador: ")
+dicionario[nome_colaborador] = cargo_colaborador
+
 for nome, cargo in dicionario.items():
     print (f"O colaborador {nome} exerce o cargo de {cargo}")
 
